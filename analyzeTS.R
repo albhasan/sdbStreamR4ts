3ac74badc14569d707364526ed076075b8528f5e
albhasan <albhasan@gmail.com>
2017-09-27 11:00:41 -0300

    scripts sucessfully tested on server

--- analyzeTS.R
+++ analyzeTS.R
@@ -1,14 +1,36 @@
+################################################################################
+# ANALYZE A TIME SERIES
 #-------------------------------------------------------------------------------
-## load test chunks from the data directory. i.e.
-# load("data/input.df-38447140")
+# NOTES: 
+# - This script is meant to be call by main.R, hence it has the same workspace. 
+#   This can be used to load resoruces such as data and other scripts
+# - It must process one single time series of data
+# - It must implement the function analyzeTS
 #-------------------------------------------------------------------------------
+# DEBUG: 
+# - Sample data is provided in the binary files in the data directory. These 
+#   sample data are chunks extracted from SciDB and they correspond to the format 
+#   STREAM sends them to R. For example load("data/input.df-38447140")
+# - The expression write("A debug message", stderr()) writes to SciDB' error log
+#   i.e /home/scidb/data/0/0/scidb-stderr.log
+################################################################################
 
 # Analyze a time-series
 #
 # @param ts.df      A data.frame. Each row is an observation
 # @return           A data.frame with one row and N columns made of atomic values
 analyzeTS <- function(ts.df){
+  dmess <- paste("-------------\n analyzeTS() got a data frame of", nrow(ts.df), 
+                 "observations of the variables",  
+                 paste(colnames(ts.df), collapse = ", "))
+  write(dmess, stderr())                                                        # write the working directory to /home/scidb/data/0/0/scidb-stderr.log
   json.dat <- jsonlite::read_json("data.json")                                  # read data as main.R sets up the working directory
-  res.df <- data.frame(cid = ts.df$cid[1], rid = ts.df$rid[1], num_row = nrow(ts.df), num_col = ncol(ts.df), stuff = "I'm so cool", len_json = length(json.dat))
+  # Build a response data.frame with the column, the row, the number of rows and 
+  # columns and the length of the json file
+  res.df <- data.frame(cid = ts.df$cid[1], 
+                       rid = ts.df$rid[1], 
+                       num_row = nrow(ts.df), 
+                       num_col = ncol(ts.df), 
+                       len_json = length(json.dat))
   return(res.df)
 }
--- main.R
+++ main.R
@@ -2,44 +2,32 @@
 # GET THE SCIDB CHUNKS PROVIDED BY SCIDB STREAMING
 #-------------------------------------------------------------------------------
 # NOTES: 
+# - Use full paths when calling R scripts
+# - It doesn't matter if the R scripts' directory is shared among all the 
+#   machines in the SciDB cluster
+# - The working directory of the main.R script is the same of the script_name.
+#   This way, the user provided script can read data and scripts 
+# - R packages must be installed in all machines in the SciDB cluster before 
+#   calling the main script
+#-------------------------------------------------------------------------------
+# DEBUG: 
+# - The expression write("A debug message", stderr()) writes to SciDB' error log
+#   i.e /home/scidb/data/0/0/scidb-stderr.log
+#-------------------------------------------------------------------------------
+# USAGE:
+# - The amount and length of time series is controled by the BETWEEN clause
 #
-# example 
-# Rscript main.R script_folder=/home/scidb/shared/query201706051451-5586 script_name=analyzeTS.R
+## EXAMPLE 1: A SciDB query return properties of a single time series
+# iquery -aq "stream(cast(project(apply(between(mod13q1_512, 62400, 43200, 0, 62400, 43200, 15), cid, col_id, rid, row_id, tid, time_id), cid, rid, tid, evi, quality, reliability), <cid:int32, rid:int32, tid:int32, evi:int32, quality:int32, reliability:int32> [col_id=0:172799:0:40; row_id=0:86399:0:40; time_id=0:511:0:512]), 'Rscript /home/scidb/shared/scripts/sdbstream/main.R script_folder=/home/scidb/shared/scripts/sdbstream script_name=analyzeTS.R', 'format=df', 'types=int32,int32,int32,int32,int32,int32')"
+## Response
+# {instance_id,chunk_no,value_no} a0,a1,a2,a3,a4,a5
+# {0,0,0} 62400,43200,7,6,1,10
 #
-# example SciDB stream query:
-# iquery -aq "
-# store(
-#   stream(
-#     cast(
-#       project(
-#         apply(
-#           between(mod13q1_512, 62400, 43200, 0, 62500, 43300, 367), 
-#          cid, col_id, rid, row_id, tid, time_id
-#         ), 
-#         cid, rid, tid, evi, quality, reliability
-#       ), 
-#      <cid:int32, rid:int32, tid:int32, evi:int32, quality:int32, reliability:int32> [col_id=0:172799:0:40; row_id=0:86399:0:40; time_id=0:511:0:512]
-#     ), 
-#     'Rscript main.R script_folder=/home/scidb/shared/query201706051451-5586 script_name=analyzeTS.R', 
-#    'format=df', 'types=int32,int32,string'
-#  ),
-#  query201706051451-5586
-# )"
-#-------------------------------------------------------------------------------
-# run bfast monitor
-#-------------------------------------------------------------------------------
-# time iquery -aq "store(stream(cast(project(apply(between(mod13q1_512, 62400, 43200, 0, 62439, 43239, 511), cid, col_id, rid, row_id, tid, time_id), cid, rid, tid, evi, quality, reliability), <cid:int32, rid:int32, tid:int32, evi:int32, quality:int32, reliability:int32> [col_id=0:172799:0:40; row_id=0:86399:0:40; time_id=0:511:0:512]), 'Rscript main.R script_folder=/home/scidb/shared/scripts/sdbStreamR4ts script_name=bfastMonitor.R', 'format=df', 'types=double,string'), test_bfastMonitor)"
-#-------------------------------------------------------------------------------
-# TODO:
-# - how does one load packages?
-#-------------------------------------------------------------------------------
-# FAQ:
-# - What is WTSPS? Web Time Series Processing Service
+## EXAMPLE 2: A SciDB query return properties of 9 time series, (col_id, row_id, time_id)
+##            from (62400, 43200, 0) to (62402, 43202, 15) 
+# iquery -aq "stream(cast(project(apply(between(mod13q1_512, 62400, 43200, 0, 62402, 43202, 15), cid, col_id, rid, row_id, tid, time_id), cid, rid, tid, evi, quality, reliability), <cid:int32, rid:int32, tid:int32, evi:int32, quality:int32, reliability:int32> [col_id=0:172799:0:40; row_id=0:86399:0:40; time_id=0:511:0:512]), 'Rscript /home/scidb/shared/scripts/sdbstream/main.R script_folder=/home/scidb/shared/scripts/sdbstream script_name=analyzeTS.R', 'format=df', 'types=int32,int32,int32,int32,int32,int32')"
 #
-# - How do users load data files (i.e json)? This script sets the working 
-#     directory to the users' WTSPS request folder. That way, the users' script 
-#     load data just using the name of the file 
-#     json.dat <- jsonlite::read_json("data.json")
+# TODO: bfast example
 ################################################################################
 #-------------------------------------------------------------------------------
 # paramteres
@@ -80,41 +68,25 @@ while( TRUE )
     flush(con_out)
     break
   }
-  #
-  input.df = as.data.frame(input_list)
-  #-----------------------------------------------------------------------------
-  # save chunks as Rdata with randomized names
-  #-----------------------------------------------------------------------------
-  #save(input.df, file = file.path(script_folder, paste("input.df", sample(10000000:99000000, 1, replace = TRUE), sep = "-"), fsep = .Platform$file.sep))
-  #-----------------------------------------------------------------------------
-  # local test 
-  # NOTE: comment before running the real deal
-  #-----------------------------------------------------------------------------
-  #script_name <- "bfast.R"
-  #script_folder <- "/home/alber/Documents/ghProjects/sdbStreamR4ts"
-  #load(file.path(script_folder, "data/input.df-27271652", fsep = .Platform$file.sep))
-  #num_cores = getOption("mc.cores", 2L)                                         # use all the cores
-  #write(jsonlite::toJSON(data.frame(x = rnorm(10), y = rnorm(10), z = rnorm(10))), file = "data.json")
+  input.df = as.data.frame(input_list, stringsAsFactors = F)
   #-----------------------------------------------------------------------------
   # configuration
   #-----------------------------------------------------------------------------
-  # 32 cores per machine
-  # 7 SciDB instances per machine
-  # leave at least one core free for the OS
-  num_cores = 32 - 7 - 1 # num_cores = getOption("mc.cores", 2L)
+  # Each machine in the cluster has 32 cores and 7 SciDB instances
+  # Leave at least one core free for the OS
+  num_cores = 32 - 7 - 1 # num_cores <- parallel::detectCores()
   setwd(script_folder)
-  source(file.path(script_folder, script_name, fsep = .Platform$file.sep))
   #-----------------------------------------------------------------------------
   # load the user's code
   #-----------------------------------------------------------------------------
-  #for(f in list.files(path = script_folder, pattern = "\\.R$")){source(file.path(script_folder, f, fsep = .Platform$file.sep))} # load ALL the script files
+  source(file.path(script_folder, script_name, fsep = .Platform$file.sep))
   if(!("analyzeTS" %in% ls())){
     stop("The function analyzeTS() was not found!")
   }
   #-----------------------------------------------------------------------------
   # call the script on each time-series of the chunk
   #-----------------------------------------------------------------------------
-  crid.df <- unique(input.df[c("cid", "rid")])                                  # get unique pairs of col & rows
+  crid.df <- unique(input.df[c("cid", "rid")])
   res_script <- parallel::mclapply(1:nrow(crid.df), 
                                    mc.cores = num_cores, 
                                    FUN = function(x, crid.df, input.df){
@@ -151,7 +123,7 @@ while( TRUE )
     return(x)
   })
   #-----------------------------------------------------------------------------
-  # return top SciDB
+  # return to SciDB
   #-----------------------------------------------------------------------------
   writeBin(serialize(res_list, NULL, xdr=FALSE), con_out)
   flush(con_out)
